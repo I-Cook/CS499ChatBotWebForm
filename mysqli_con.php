68504db5bc7a9db99c06d674997dff803db586ea
I-Cook <john.cook6@uky.edu>
2018-04-24 18:54:00 -0400

    testing connection

--- mysqli_con.php
+++ mysqli_con.php
@@ -7,6 +7,7 @@
        $conn = new mysqli($servername, $username, $password);
        if ($conn->connect_error){
 	die("Connection failed: " . $conn->connect_error);
+	echo "Fail Connect.";
        }
 
        //create the query to be passed
