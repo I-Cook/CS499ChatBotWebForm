924e7d410abcdec1b1ff9f3d36bc8dbcabb8f129
JacobPawlak <jacob.pawlak@uky.edu>
2018-04-26 16:17:27 -0400

    row

--- Login/signIn.php
+++ Login/signIn.php
@@ -13,6 +13,8 @@
 
 <body class="text-center">
 
+<div class="row">
+
 <div class="col-md-4"></div>
 
 <div class="col-md-4">
@@ -30,6 +32,8 @@
 </div>
 
 <div class="col-md-4"></div>
+
+</div>
 
 </body>
 </html>
