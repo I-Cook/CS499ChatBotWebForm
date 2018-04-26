48a09c9ad1c11de513cf13c06c707b52327ec99a
JacobPawlak <jacob.pawlak@uky.edu>
2018-04-26 16:05:33 -0400

    stuff

--- mysqli_con.php
+++ mysqli_con.php
@@ -6,7 +6,6 @@
        echo "Initializing";
        //<!-- create the connection -->
        $conn = new mysqli($servername, $username, $password);
-	echo $conn;
        echo "Connection done";
        if ($conn->connect_error){
 	die("Connection failed: " . $conn->connect_error);
