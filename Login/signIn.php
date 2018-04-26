964c8e0bf78396697fb99632095394c23bd15737
JacobPawlak <jacob.pawlak@uky.edu>
2018-04-26 16:12:50 -0400

    added columns

--- Login/signIn.php
+++ Login/signIn.php
@@ -8,12 +8,16 @@
 <title>Sign in for chatbot</title>
 
 <link href="bootstrap/dist/css/bootstrap.min.css" rel="stylesheet">
-
 <link href="CSS/signin.css" rel="stylesheet">
 </head>
 
 <body class="text-center">
+
+<div class="col-md-4"></div>
+
+<div class="col-md-4">
 <form method="post" action='../mysqli_con.php'>
+
 <h1 class="h2 mb-3 font-weight-normal">Anyware Chatbot</h1>
 <h2 class="h3 mb-3 font-weight-normal">Please Sign In</h2>
 <label for="inputUsername" class="sr-only">Username</label>
@@ -23,5 +27,9 @@
 <button class="btn btn-lg btn-primary btn-block" type="submit">Sign In</button>
 
 </form>
+</div>
+
+<div class="col-md-4"></div>
+
 </body>
 </html>
